b5328eb832b6f8e57ff371bea6009d840be8fd0a
neelchheda <64920222+neelchheda@users.noreply.github.com>
2020-05-07 12:18:33 +0530

    Add files via upload

---
+++ mainpage.php
@@ -0,0 +1,22 @@
+<html lang="en" >
+	<head>
+		<meta charset="utf-8">
+		<title></title>
+		<link rel="stylesheet" href="firstpagestyler.css">
+	</head>
+	<body>
+		<div class="container">
+			<form action="login.php" method="post" autocomplete="off">
+				<button class="btn btn1" onclick="login.php">Login</button>
+				
+			</form>
+			<form action="register.php" method="post" autocomplete="off">
+				<button class="btn btn2" onclick="register.php">Register</button>
+			</form>
+			
+		</div>
+	</body>
+</html>
+
+
+
